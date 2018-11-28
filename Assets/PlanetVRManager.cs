a4087c9ab621eca411f3514f3e3d672c28bcc112
ingridwarrior2901 <ingridguerrero@protonmail.com>
2018-11-28 18:13:21 -0500

    first commit

---
+++ Assets/PlanetVRManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetVRManager : MonoBehaviour
+{
+    [Header("VR Game Objects")]
+    public GameObject controllerMain;
+    public GameObject editorEmulator;
+
+    [Header("Camera Game Objects")]
+    public GameObject vrCamera;
+    public GameObject orbitCamera;
+   
+    public bool useVR = false;
+
+    private GameObject m_controllerMain;
+    private GameObject m_editorEmulator;
+
+    void Start()
+    {
+        m_controllerMain = Instantiate(controllerMain);
+        m_editorEmulator = Instantiate(editorEmulator);
+    }
+
+    void Update()
+    {
+        OnVRSettingsChange();
+    }
+
+    void OnVRSettingsChange() 
+    {
+        orbitCamera.SetActive(!useVR);
+        VRGameObjects();
+    }
+
+    void VRGameObjects()
+    {
+        vrCamera.SetActive(useVR);
+        m_controllerMain.SetActive(useVR);
+        m_editorEmulator.SetActive(useVR);
+    }
+}
---
+++ Assets/Scripts/PlanetRotationController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetRotationController : MonoBehaviour
+{
+    [Header("Settings")]
+    [Tooltip("angular speed, R per sec.")]
+    [SerializeField] float degreesPerSeconds;
+
+    void Update()
+    {
+        RotatePlanets();
+    }
+
+    void RotatePlanets()
+    {
+        transform.rotation *= Quaternion.AngleAxis(degreesPerSeconds * Time.deltaTime, Vector3.up);
+    }
+}
